fc89cda61ec9b3adb7b99bf323b6e03d56f62197
Sainiprince27 <saprince@sheridancollege.ca>
2024-09-20 08:47:58 -0400

    Update MyfirstjavaProject.java

--- src/myfirstjavaproject/MyfirstjavaProject.java
+++ src/myfirstjavaproject/MyfirstjavaProject.java
@@ -16,6 +16,7 @@ public class MyfirstjavaProject {
     public static void main(String[] args) {
         System.out.println("Hello world");
         // TODO code application logic here
+        //cgfhjkjlkl
     }
     
 }
